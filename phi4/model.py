62f1831be2af5915f4a8b4ed6d763cf47a647cf1
Hugo Negrel <hugonegrel13@gmail.com>
2026-01-26 19:56:09 +0100

    Add model.py

---
+++ phi4/model.py
@@ -0,0 +1,295 @@
+import torch.nn as nn
+import torch
+import typing
+import math
+
+# This is the utils file
+def zero_out(layer):
+    for p in layer.parameters():
+        p.detach().zero_()
+    return layer
+
+# AdaGN according to paper "Diffusion Models Beat GANs on Image Synthesis"
+class AdaNorm(nn.Module):
+    def __init__(self, num_channel: int):
+        super().__init__()
+        num_group = int(num_channel/16) # According to group norm paper, 16 channels per group produces the best result
+        self.gnorm = nn.GroupNorm(num_group, num_channel, affine=False)
+
+    def forward(self, tensor: torch.Tensor, emb: torch.Tensor):
+        scale, shift = torch.chunk(emb, 2, dim=1)
+        tensor = self.gnorm(tensor)
+        tensor = tensor * (1 + scale) + shift
+        return tensor
+
+class MyGroupNorm(nn.Module):
+    def __init__(self, num_channel: int):
+        super().__init__()
+        num_group = int(num_channel/16) # According to group norm paper, 16 channels per group produces the best result
+        self.gnorm = nn.GroupNorm(num_group, num_channel, affine=False)
+
+    def forward(self, tensor: torch.Tensor):
+        return self.gnorm(tensor)
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channel: int, out_channel: int, emb_dim: int = 1024, up: bool = False, down: bool = False):
+        super().__init__()
+        self.emb = nn.Sequential(nn.SiLU(), nn.Linear(emb_dim, 2*out_channel))
+        if up:
+            self.change_size = nn.Upsample(scale_factor=2, mode='nearest')
+        elif down:
+            self.change_size = nn.AvgPool2d(kernel_size=2, stride=2)
+        else:
+            self.change_size = nn.Identity()
+
+        # Normalization
+        self.gnorm1 = MyGroupNorm(in_channel)
+        self.gnorm2 = AdaNorm(out_channel)
+
+        # Convolution
+        self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size = 3, padding = 1)
+        self.conv2 = nn.Conv2d(out_channel, out_channel, kernel_size = 3, padding = 1)
+        if in_channel != out_channel:
+            self.conv3 = nn.Conv2d(in_channel, out_channel, kernel_size = 1)
+        else:
+            self.conv3 = nn.Identity()
+
+        # Combine input stage
+        self.input = nn.Sequential(
+            self.gnorm1,
+            nn.SiLU(),
+            self.change_size,
+            self.conv1
+        )
+
+        # Combine output stage
+        self.output = nn.Sequential(
+            nn.SiLU(),
+            zero_out(self.conv2)
+        )
+
+        # Skip connection
+        self.skip_connection = nn.Sequential(
+            self.change_size,
+            self.conv3
+        )
+
+        # Embedding
+        self.embed = nn.Sequential(
+            nn.Linear(emb_dim, emb_dim),
+            nn.SiLU(),
+            nn.Linear(emb_dim, 2 * out_channel)
+        )
+
+    def forward(self, tensor: torch.Tensor, emb: torch.Tensor):
+        emb = self.embed(emb).view(tensor.shape[0], -1, 1, 1)
+
+        h = self.input(tensor)
+        h = self.gnorm2(h, emb)
+        h = self.output(h)
+        x = self.skip_connection(tensor)
+
+        return x + h
+
+class SelfAttention(nn.Module):
+    def __init__(self, channel: int):
+        super().__init__()
+        self.num_head = int(channel/32)
+
+    def forward(self, tensor: torch.Tensor):
+        batch, channel, length = tensor.shape
+        ch = channel // 3 // self.num_head
+        q, k, v = tensor.chunk(3, dim = 1)
+        scale = 1 / math.sqrt(math.sqrt(ch))
+        # The code below is from Diffusion Model Beat GANs on Image Synthesis paper code
+        weight = torch.einsum(
+            "bct,bcs->bts",
+            (q * scale).view(batch * self.num_head, ch, length),
+            (k * scale).view(batch * self.num_head, ch, length),
+        )  # More stable with f16 than dividing afterwards
+        weight = torch.softmax(weight.float(), dim=-1).type(weight.dtype)
+        a = torch.einsum("bts,bcs->bct", weight, v.reshape(batch * self.num_head, ch, length))
+        return a.reshape(batch, -1, length)
+
+
+class Attention(nn.Module):
+    def __init__(self, channel: int):
+        super().__init__()
+        self.gnorm = MyGroupNorm(channel)
+        self.qkv = nn.Conv1d(channel, channel * 3, 1)
+        self.attention = SelfAttention(channel)
+        self.output = zero_out(nn.Conv1d(channel, channel, 1))
+
+    def forward(self, tensor: torch.Tensor):
+        # Perform self attention
+        batch, channel, width, height = tensor.shape
+        tensor = tensor.reshape(batch, channel, -1)
+        # Skip connection
+        tensor_skip = tensor
+        tensor = self.gnorm(tensor)
+        tensor = self.qkv(tensor)
+        tensor = self.attention(tensor)
+        tensor = self.output(tensor)
+
+        # Adding the skip connection tensor back to the current tensor
+        tensor = tensor + tensor_skip
+
+        tensor = tensor.reshape(batch, channel, width, height)
+        return tensor
+
+class UNet(nn.Module):
+    def __init__(self, image_channel: int = 1, depth: int = 2, emb_dim: int = 1024):
+        super().__init__()
+
+        # Create model architecture
+        channels = [160, 320, 544, 608]
+        attention_channel = [544, 608]
+        self.encoder = nn.ModuleList([nn.ModuleList([nn.Conv2d(image_channel, channels[0], 3, padding=1)])])
+        self.decoder = nn.ModuleList()
+
+        skip_channel = [channels[0]]
+
+        # Encoder
+        for i in range(len(channels)):
+            for _ in range(depth):
+                layer = nn.ModuleList()
+                layer.append(ResBlock(channels[i], channels[i], emb_dim = emb_dim))
+                if channels[i] in attention_channel:
+                    layer.append(Attention(channels[i]))
+                self.encoder.append(layer)
+                skip_channel.append(channels[i])
+
+            if i != len(channels)-1:
+                layer = nn.ModuleList()
+                layer.append(ResBlock(channels[i], channels[i + 1], down=True, emb_dim = emb_dim))
+                self.encoder.append(layer)
+                skip_channel.append(channels[i+1])
+
+        # Bottleneck
+        self.bottle_neck = nn.ModuleList([
+            ResBlock(channels[-1], channels[-1], emb_dim = emb_dim),
+            Attention(channels[-1]),
+            ResBlock(channels[-1], channels[-1], emb_dim = emb_dim),
+        ])
+
+        # Decoder
+        for i in range(len(channels)-1, -1, -1):
+            for block in range(depth+1):
+                layer = nn.ModuleList()
+                layer.append(ResBlock(channels[i] + skip_channel.pop(), channels[i], emb_dim = emb_dim))
+                if channels[i] in attention_channel:
+                    layer.append(Attention(channels[i]))
+
+                if i != 0 and block == depth:
+                    layer.append(ResBlock(channels[i], channels[i - 1], up=True, emb_dim = emb_dim))
+
+                self.decoder.append(layer)
+
+        # Create time embedding
+        self.time_embedding = nn.Sequential(
+            nn.Linear(emb_dim, emb_dim),
+            nn.SiLU(),
+            nn.Linear(emb_dim, emb_dim)
+        )
+
+        # Output kernels to change back to image channel
+        self.out = nn.Sequential(
+            MyGroupNorm(channels[0]),
+            nn.SiLU(),
+            zero_out(nn.Conv2d(channels[0], image_channel, 3, padding=1)),
+        )
+
+    def forward(self, tensor: torch.Tensor, time_embedding: torch.Tensor):
+        # Creating embedding
+        embedding = self.time_embedding(time_embedding)
+        
+        skip_connection = []
+
+        # Encoder
+        for layer in self.encoder:
+            for module in layer:
+                if(isinstance(module, ResBlock)):
+                    tensor = module(tensor, embedding)
+                else:
+                    tensor = module(tensor)
+
+            skip_connection.append(tensor)
+
+        # Bottleneck
+        for module in self.bottle_neck:
+            if(isinstance(module, ResBlock)):
+                tensor = module(tensor, embedding)
+            else:
+                tensor = module(tensor)
+
+        # Decoder
+        for layer in self.decoder:
+            P = skip_connection.pop()
+            if P.shape[-1] == tensor.shape[-1] + 1:
+                tensor= F.pad(tensor, (1, 0, 1, 0), "constant", 0) 
+
+            tensor = torch.concatenate((tensor, P), dim = 1)
+            for module in layer:
+                if(isinstance(module, ResBlock)):
+                    tensor = module(tensor, embedding)
+                else:
+                    tensor = module(tensor)
+
+        tensor = self.out(tensor)
+
+        return tensor
+
+
+class VelocityFieldImage(nn.Module):
+    def __init__(self, init_features=28, device='cpu'):
+        super(VelocityFieldImage, self).__init__()
+        # Increment the input channels by 1 to accommodate the alpha dimension
+        self.unet = UNet(image_channel = 1, depth = 2, emb_dim = init_features**2).to(device)
+
+    def forward(self, x, alpha):
+        """
+        Important Note: alpha should be a linear vector, and not a matrix.
+        """
+        return self.unet(x, alpha)
+
+class ODE:
+    def __init__(self, b: VelocityFieldImage, interpolant, n_step: int, Alpha: torch.Tensor = None, mask: torch.Tensor = None, device: str = 'cpu'):
+        """
+        Attributes:
+            b: VectorField. Generator of the flow.
+            interpolant: Interpolant. This is the interpolant between source and target distribution.
+            n_step: int. Step size for the resolution of the ODE.
+            Alpha : Tensor. It is the trajectory in the alpha-space of the stochastic interpolant.
+            mask: Tensor. Mask the generator for the non-corrupted entries.
+        """
+        self.b           = b
+        self.interpolant = interpolant
+        self.n_step      = n_step
+        self.device = device
+        if Alpha is None:
+            self.alpha       = torch.stack((torch.linspace(1.0,0.0, n_step + 1, device = device),)*dim, axis = 1)
+        else:
+            self.alpha = Alpha
+        self.dt          = 1/n_step
+        if mask is None:
+            self.mask = torch.ones(self.alpha.shape[-1], device = device)
+        else:
+            assert len(mask) == self.alpha.shape[-1], "The mask should have the shape of the data space"
+            self.mask = mask
+            if mask.device != device:
+                self.mask = mask.to(device)
+
+    def step(self, x: torch.Tensor, alpha: torch.Tensor) -> torch.Tensor:
+        mask = self.mask.reshape(*x.shape[1:])
+        gen = self.b(x, alpha)
+        return x - 1*mask*gen*self.dt
+    
+    def solve(self, x_init: torch.Tensor) -> torch.Tensor:
+        self.b.eval()
+        xs = torch.zeros((self.n_step, *x_init.shape), device = device)
+        x = x_init
+        for i, alpha in enumerate(self.alpha[:-1]):
+            alpha = alpha.repeat(len(x), 1) #Repeat for each batch size. alpha should be of size [batch_size, dim] at this point.
+            x = self.step(x, alpha)
+            xs[i] = x
+        return xs
